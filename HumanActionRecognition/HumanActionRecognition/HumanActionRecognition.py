e83737eb7dbd645d24a3a929e6647f1d7403be7b
Flogo345 <Flogo345@web.de>
2020-04-25 14:22:30 +0200

    load openpose test

--- HumanActionRecognition/HumanActionRecognition/HumanActionRecognition.py
+++ HumanActionRecognition/HumanActionRecognition/HumanActionRecognition.py
@@ -4,15 +4,26 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import random
+import caffemodel2pytorch
 from torchvision import transforms
 from PIL import Image
 from torch.autograd import Variable
 from os import listdir
 
-if os.path.isfile('openpose.pt'):
-    model = torch.load('openpose.pt')
+#if os.path.isfile('openpose.pt'):
+#    model = torch.load('openpose.pt')
 
-input = get_input()
-input = preprocess_input(input)
+model = caffemodel2pytorch.Net(
+	prototxt = 'VGG_ILSVRC_16_layers_deploy.prototxt',
+	weights = 'VGG_ILSVRC_16_layers.caffemodel')
+
 model.eval()
-out = model(input)
+torch.set_grad_enabled(False)
+img = Image.open('random.jpg')
+out = model(img)
+
+print (out)
+#input = get_input()
+#input = preprocess_input(input)
+#model.eval()
+#out = model(input)
